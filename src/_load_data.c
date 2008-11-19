541506bf72a7df9bdedb7eaa35aaab5f2d24cf86
mmh1 <devnull@localhost>
2008-11-19 22:56:09 +0000

    *** empty log message ***

--- src/_load_data.c
+++ src/_load_data.c
@@ -90,11 +90,12 @@ herr_t supercontig_visitor(hid_t g_id, const char *name,
   subgroup = H5Gopen(g_id, name, H5P_DEFAULT);
   assert(subgroup >= 0);
 
-  fprintf(stderr, " %s: %d\n", name, subgroup);
-
   get_attr(subgroup, ATTR_START, H5T_STD_I32LE, &supercontig->start);
   get_attr(subgroup, ATTR_END, H5T_STD_I32LE, &supercontig->end);
   supercontig->group = subgroup;
+
+  fprintf(stderr, " %s (%d, %d): %d\n", name, supercontig->start,
+          supercontig->end, subgroup);
 
   return 0;
 }
@@ -410,7 +411,8 @@ int main(void) {
         *buf_ptr++ = datum;
       } /* else: ignore data until we get to another header line */
     } else {
-      write_buf(h5file, buf_start, buf_end, buf_filled_start, buf_ptr, &supercontigs);
+      write_buf(h5file, buf_start, buf_end, buf_filled_start, buf_ptr,
+                &supercontigs);
       proc_wigfix_header(line, &h5file, &supercontigs,
                          &buf_start, &buf_end, &buf_ptr);
       buf_filled_start = buf_ptr;
