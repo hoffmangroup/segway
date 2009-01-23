cb6e5f40555109c4f1bc7863254eeed397185f32
mmh1 <devnull@localhost>
2009-01-23 22:43:25 +0000

    change install.sh to release.sh

--- release.sh
+++ release.sh
@@ -4,7 +4,11 @@ PACKAGE=$(python -c 'import setup; print setup.name')
 VERSION=$(python -c 'import setup; print setup.__version__')
 PREFIX="$MOD_NOBLESW/$PACKAGE/$VERSION/$MODULES_OS/$MODULES_REL/$MODULES_MACH"
 
-svn cp 
+svn commit
+svn cp . $(svn info | grep "^URL: " \
+    | perl -pe 's/^URL: //; s/trunk$/tags\/release-/')$VERSION
+
+python setup.py release
 
 python setup.py install --prefix="$PREFIX" \
     --install-lib="$PREFIX/lib/python$PYTHON_VERSION" \
