6f4b5958f0518d79b0b8fd892219af9d2a633529
mmh1 <devnull@localhost>
2009-01-27 23:16:20 +0000

    checkpoint

--- release.sh
+++ release.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 
-PACKAGE=$(python -c 'import setup; print setup.name')
-VERSION=$(python -c 'import setup; print setup.__version__')
+# XXX: do for each platform
+PACKAGE="$(python setup.py --name)"
+VERSION="$(python setup.py --version)"
 PREFIX="$MOD_NOBLESW/$PACKAGE/$VERSION/$MODULES_OS/$MODULES_REL/$MODULES_MACH"
-
-svn commit
-svn cp . $(svn info | grep "^URL: " \
-    | perl -pe 's/^URL: //; s/trunk$/tags\/release-/')$VERSION
-
-python setup.py release
-
 python setup.py install --prefix="$PREFIX" \
     --install-lib="$PREFIX/lib/python$PYTHON_VERSION" \
     --install-scripts="$PREFIX/bin" \
     --single-version-externally-managed --record=dist/record.txt
 
-echo "increment the release"
-emacs setup.py
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 LONG_DESCRIPTION
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # Copyright 2008-2009 Michael M. Hoffman <mmh1@washington.edu>
 
