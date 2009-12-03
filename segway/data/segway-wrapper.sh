a191892ba68da62c04e44da8f007324df1ecfc96
mmh1 <devnull@localhost>
2009-12-03 01:34:26 +0000

    fix ulimit option error

--- segway/data/segway-wrapper.sh
+++ segway/data/segway-wrapper.sh
@@ -3,7 +3,9 @@
 # first argument is memory limit in kibibytes
 
 # -c 0: no core dump files
-ulimit -c 0 -v "$1" -M "$1" || exit 201
+# -v: virtual memory
+# -m: per process memory limit (no effect on Linux?)
+ulimit -c 0 -v "$1" -m "$1" || exit 201
 shift
 
 export TMPDIR="$(mktemp -dt segway.XXXXXXXXXX)"
