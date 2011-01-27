72b53e2a3a61497ed544bf1649131f7062be8bf9
mmh1 <devnull@localhost>
2011-01-27 00:42:00 +0000

    * segway: SGE: fix bug where Viterbi jobs would immediately crash with   a pthreads error when using recent versions of PyTables/numexpr

--- segway/cluster/sge.py
+++ segway/cluster/sge.py
@@ -6,6 +6,7 @@ __version__ = "$Revision$"
 # Copyright 2009, 2011 Michael M. Hoffman <mmh1@washington.edu>
 
 from math import ceil
+from resource import getrlimit, RLIMIT_STACK
 import sys
 
 from .._util import MB
@@ -14,11 +15,23 @@ from .common import _JobTemplateFactory, make_native_spec
 # qsub -w: switches off job validation
 NATIVE_SPEC_DEFAULT = dict(w="n")
 
+try:
+    STACK_LIMIT = min(num for num in getrlimit(RLIMIT_STACK)
+                      if num > 0)
+except ValueError:
+    STACK_LIMIT = 10*MB
+
 class JobTemplateFactory(_JobTemplateFactory):
     def make_res_req(self, mem_usage, tmp_usage):
         # tmp_usage unused on SGE
+
+        # by default the stack limit is so high that there is an
+        # automatic crash when you also set a virtual memory limit and
+        # then start new pthreads. setting h_stack is necessary if you
+        # are setting h_vmem
         return [make_single_res_req("mem_requested", mem_usage),
-                make_single_res_req("h_vmem", self.mem_limit)]
+                make_single_res_req("h_vmem", self.mem_limit),
+                make_single_res_req("h_stack", STACK_LIMIT)]
 
     def make_native_spec(self):
         # qsub -l: resource requirement
