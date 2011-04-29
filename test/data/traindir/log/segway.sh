9b6ec6fc606aebfac1c435ffa07c4aa05c750424
mmh1 <devnull@localhost>
2011-04-29 00:35:06 +0000

    reduce number of training rounds

--- test/data/traindir/log/segway.sh
+++ test/data/traindir/log/segway.sh
@@ -1,3 +1,3 @@
 ## segway (%[^ ]+%) run (%[0-9a-f]{32}%) at (%[0-9]{4}%)-(%[0-9]{2}%)-(%[0-9]{2}%) (%[0-9]{2}%):(%[0-9]{2}%):(%[0-9]{2}%).(%[0-9]{1,}%)
 
-"(%[^"]+%)/segway" "--num-labels=4" "--input-master=../data/input.master" "train" "../data/test.genomedata" "traindir"
+"(%[^"]+%)/segway" "--num-labels=4" "--max-train-rounds=2" "--input-master=../data/input.master" "train" "../data/test.genomedata" "traindir"
