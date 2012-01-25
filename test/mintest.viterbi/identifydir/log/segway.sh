63fd3d7b5fd4b224374cdfe6a57896ace084d0a9
mmh1 <devnull@localhost>
2012-01-25 23:57:39 +0000

    delete mintest.viterbi

--- test/mintest.viterbi/identifydir/log/segway.sh
+++
@@ -1,3 +0,0 @@
-## segway 1.2.0.dev-r6121 run af887cb0beef11e09c1c00237d9dcf3a at 2011-08-05 00:15:47.420673
-
-"/homes/hoffman/arch/Linux-x86_64/bin/segway" "identify+posterior" "../data/test.genomedata" "traindir" "identifydir"
--- test/mintest.viterbi/traindir/log/segway.sh
+++
@@ -1,3 +0,0 @@
-## segway 1.2.0.dev-r6121 run 7335ae40beef11e0938400237d9dcf3a at 2011-08-05 00:14:06.105779
-
-"/homes/hoffman/arch/Linux-x86_64/bin/segway" "--num-labels=4" "--max-train-rounds=2" "--input-master=../data/input.master" "train" "../data/test.genomedata" "traindir"
