a5419d21f00167c51413dff21b053a08c5b8e3a7
mmh1 <devnull@localhost>
2012-02-08 22:53:48 +0000

    * test/test.sh: replace NEW, with SEGWAY_RAND_SEED * test/data: new version of data generated with SEGWAY_RAND_SEED

--- test/test.sh
+++ test/test.sh
@@ -17,21 +17,15 @@ TMPDIR="$(mktemp -dp . "test-$(date +%Y%m%d).XXXXXX")"
 echo >&2 "entering directory $TMPDIR"
 cd "$TMPDIR"
 
-# generate a new input master: don't expect the files to actually
-# match, but expect things to work without crashing
-if [ "${NEW:-}" ]; then
-    input_master_arg=""
-else
-    input_master_arg="--input-master=../data/input.master"
-fi
-
-if [ "${CLUSTER_OPT:-}" ]; then
-    cluster_arg="--cluster-opt=$CLUSTER_OPT"
+if [ "${SEGWAY_TEST_CLUSTER_OPT:-}" ]; then
+    cluster_arg="--cluster-opt=$SEGWAY_TEST_CLUSTER_OPT"
 else
     cluster_arg="--cluster-opt="
 fi
 
-segway --num-labels=4 --max-train-rounds=2 $input_master_arg "$cluster_arg" \
+# seed from random.randrange(2**32)
+SEGWAY_RAND_SEED=203078386 segway --num-labels=4 --max-train-rounds=2 \
+    "$cluster_arg" \
     train ../data/test.genomedata traindir
 segway "$cluster_arg" \
     identify+posterior ../data/test.genomedata traindir identifydir
