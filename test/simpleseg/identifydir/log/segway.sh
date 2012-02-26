70dd4af584ed0f5a25a6d31de743bc4701bfa6c4
maxwl <devnull@localhost>
2012-02-26 21:04:54 +0000

    adding simpleseg

---
+++ test/simpleseg/identifydir/log/segway.sh
@@ -0,0 +1,4 @@
+## segway 1.2.0.dev-r7532 run 28382ebc60bc11e18c0a0019b9e18917 at 2012-02-26 12:55:04.317542
+
+cd "/net/noble/vol2/home/maxwl/simpleseg"
+"/net/noble/vol2/home/maxwl/arch/Linux-x86_64/bin/segway" "identify" "simpleseg.genomedata" "traindir" "identifydir"
---
+++ test/simpleseg/run-segway.sh
@@ -0,0 +1,2 @@
+segway train --include-coords=include-coords.bed --tracks-from=tracks.txt --num-labels=4 simpleseg.genomedata traindir
+segway identify simpleseg.genomedata traindir identifydir
