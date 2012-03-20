5efde4fbe5419e1818950edc6d31561c53cc4cd1
mmh1 <devnull@localhost>
2012-03-20 21:33:05 +0000

    fix

--- test/simpleseg/run.sh
+++ test/simpleseg/run.sh
@@ -12,6 +12,8 @@ if [ $# != 0 ]; then
     exit 2
 fi
 
+datadir="$(dirname $0)"
+
 TMPDIR="$(mktemp -dp . "test-$(date +%Y%m%d).XXXXXX")"
 
 echo >&2 "entering directory $TMPDIR"
@@ -27,11 +29,11 @@ set -x
 
 # seed from python -c "import random; print random.randrange(2**32)"
 SEGWAY_RAND_SEED=1498730685 segway "$cluster_arg" \
-    --include-coords=../include-coords.bed \
-    --tracks-from=../tracks.txt --num-labels=4 \
-    train ../simpleseg.genomedata traindir
+    --include-coords="../$datadir/include-coords.bed" \
+    --tracks-from="../$datadir/tracks.txt" --num-labels=4 \
+    train "../$datadir/simpleseg.genomedata" traindir
 
-segway "$cluster_arg" --include-coords=../include-coords.bed \
-    identify ../simpleseg.genomedata traindir identifydir
+segway "$cluster_arg" --include-coords="../$datadir/include-coords.bed" \
+    identify "../$datadir/simpleseg.genomedata" traindir identifydir
 
-../../compare_directory.py ../touchstone .
+../compare_directory.py "../$datadir/touchstone" .
