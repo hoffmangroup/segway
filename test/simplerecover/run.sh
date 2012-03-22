8f2850fbb99f3aeb65386e1055b818fd1cc3b1a8
epaul9 <devnull@localhost>
2012-03-22 22:44:10 +0000

    Simple recover test added. It will test segway's recovery functionality. It is currently incomplete.

---
+++ test/simplerecover/run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+## test.sh: test segway
+## run this from the parent
+
+## $Revision: -1 $
+
+set -o nounset -o pipefail -o errexit
+
+if [ $# != 0 ]; then
+    echo usage: "$0"
+    exit 2
+fi
+
+TMPDIR="$(mktemp -dp . "test-$(date +%Y%m%d).XXXXXX")"
+
+echo >&2 "entering directory $TMPDIR"
+cd "$TMPDIR"
+
+if [ "${SEGWAY_TEST_CLUSTER_OPT:-}" ]; then
+    cluster_arg="--cluster-opt=$SEGWAY_TEST_CLUSTER_OPT"
+else
+    cluster_arg="--cluster-opt="
+fi
+
+set -x
+
+# seed from python -c "import random; print random.randrange(2**32)"
+# SEGWAY_RAND_SEED=1498730685 segway "$cluster_arg" \
+#     --include-coords=../include-coords.bed \
+#     --tracks-from=../tracks.txt --num-labels=4 \
+#     train ../simpleseg.genomedata traindir
+
+#segway "$cluster_arg" --include-coords=../include-coords.bed \
+    #identify ../simpleseg.genomedata traindir identifydir
+
+#../../compare_directory.py ../touchstone .
