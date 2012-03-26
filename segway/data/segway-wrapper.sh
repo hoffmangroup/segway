aebd17f85973dac8a34b4cbc2f3c6a1118df8e2c
mmh1 <devnull@localhost>
2012-03-26 22:08:19 +0000

    segway/data/segway-wrapper.sh style changes

--- segway/data/segway-wrapper.sh
+++ segway/data/segway-wrapper.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 ## memory limit in kibibytes
-MEMLIMIT="$1"
+mem_limit_kb="$1"
 
 ## the original temporary dir used by the submitting program (usually is /tmp)
-SUBMIT_TMPDIR="$2"
+submit_tmpdir="$2"
 shift 2
 
 # -c 0: no core dump files
 # -v: virtual memory
 # -m: per process memory limit (no effect on newer Linuxes)
-ulimit -c 0 -v "$MEMLIMIT" -m "$MEMLIMIT" || exit 201
+ulimit -c 0 -v "$mem_limit_kb" -m "$mem_limit_kb" || exit 201
 
 if [ "${LSB_JOBID:-}" ]; then
     # this way, the post-exec script can delete it easily
@@ -23,9 +23,9 @@ on_exit ()
 {
     rm -rf "$TMPDIR"
 
-    # delete any arguments that begin with $SUBMIT_TMPDIR
-    for ARG in "$@"; do
-        if [[ "$ARG" == "$SUBMIT_TMPDIR"* ]]; then
+    # delete any arguments that begin with $submit_tmpdir
+    for arg in "$@"; do
+        if [[ "$arg" == "$submit_tmpdir"* ]]; then
             rm -rf "$TMPDIR" 2>/dev/null || true
         fi
     done
