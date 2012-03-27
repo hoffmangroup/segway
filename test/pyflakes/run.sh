d97b269fe79fb17e7b07513192f600b9aa40cbb2
epaul9 <devnull@localhost>
2012-03-27 20:59:57 +0000

    pyflakes test is responsible for running pyflakes on every python file

---
+++ test/pyflakes/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o nounset -o pipefail -o errexit
+
+if [ $# != 0 ]; then
+    echo usage: "$0"
+    exit 2
+fi
+
+PYFLAKES_PATH=$(type -P pyflakes) || true
+
+if [ -z $PYFLAKES_PATH ]; then
+    echo "ERROR: Cannot find pyflakes installation."
+    echo "skipping pyflakes check"
+    exit 127
+fi
+
+#find ../.. -name "*.py" -exec ./flymake-pyflakes.sh {} +
