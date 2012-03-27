54e6982989ef0edace0c684f13a7aa9bd2ae694a
epaul9 <devnull@localhost>
2012-03-27 21:20:55 +0000

    Testing Revision property

--- test/pyflakes/run.sh
+++ test/pyflakes/run.sh
@@ -3,7 +3,7 @@
 # Checks all .py files in segway install using ./flymake-pyflakes script
 # which itself should run pyflakes on all of the python files.
 # $Revision$
-# Copyright 2010, 2011 Michael M. Hoffman <mmh1@uw.edu>
+# Copyright 2010, 2011, 2012 Michael M. Hoffman <mmh1@uw.edu>
 
 set -o nounset -o pipefail -o errexit
 
