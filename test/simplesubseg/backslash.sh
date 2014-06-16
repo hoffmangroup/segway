87c6d992c70e24893caf378253f71b3d4135a566
ajshaw <ajshaw@uchicago.edu>
2014-06-16 11:56:32 -0500

    Leaving out changes to test/ for now

--- test/simplesubseg/backslash.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-while IFS= read -rd '' file; do
-  dirname="${file%/*}"
-  basename="${file##*/}"
-  mv --backup=numbered -- "${file}" "${dirname}/${basename//\\/_}"
-done < <(find /full/path/to/dir -depth -name '*\\*' -print0)
--- test/simplesubseg/change.sh
+++
@@ -1,3 +0,0 @@
-for filename in *; do
-  echo ____ PROGRAM ENDED SUCCESSFULLY WITH STATUS 0 AT \(%[^_]+%\)____ >> $filename
-done
