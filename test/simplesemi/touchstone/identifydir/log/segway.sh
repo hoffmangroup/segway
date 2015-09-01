e87f4e307d0b7017db16076036ac60108fe3bd5f
Tamaki_Sakura <tamaki.sakura@hotmail.com>
2015-09-01 16:13:38 -0400

    Commit with Add Remove

---
+++ test/simplesemi/touchstone/identifydir/log/segway.sh
@@ -0,0 +1,4 @@
+## segway 1.2.3.dev0 run 265c6c982d7d11e5b118989096b2d04e at 2015-07-18 14:45:24.268637
+
+cd "/scratch/xzeng/segway/test/simplesemi/test-20150718.QARU2q"
+"/users/xzeng/.local/bin/segway" "--cluster-opt=" "--include-coords=../include-coords.bed" "identify" "../simplesemi.genomedata" "traindir" "identifydir"
