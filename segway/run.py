85c2d142ef0a6030a508b15e9c4facae30375d7a
mickaelm <mickaelm@mhoffman8.uhnres.utoronto.ca>
2016-02-11 11:26:10 -0500

    auto adjust the ruler-scale only if a rule-scale is not specified by the user

--- segway/run.py
+++ segway/run.py
@@ -758,7 +758,8 @@ class Runner(object):
 
         # If the resolution is set to a non-default value
         # And the ruler has not been set from the options
-        if res.resolution != RESOLUTION and options.ruler_scale:
+        if (res.resolution != RESOLUTION and
+            not options.ruler_scale):
             # Set the ruler scale to 10x the non-default value
             res.ruler_scale = res.resolution * 10
         # Else if the ruler is not divisible by the resolution
