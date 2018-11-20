9b6447552e74c6c6a03097e25607dd389348cb08
mjmm13 <devnull@localhost>
2018-11-20 12:50:05 -0500

    fixed run.sh for indetify steps

--- test/simpleidentifysteps/run.sh
+++ test/simpleidentifysteps/run.sh
@@ -33,12 +33,12 @@ segway "$cluster_arg" identify-init  --include-coords="../include-coords.bed" \
     "../simpleseg.genomedata" traindir identifydir
     
     
-python${SEGWAY_TEST_PYTHON_VERSION:-""} ../touchstone_init ./
+python${SEGWAY_TEST_PYTHON_VERSION:-""} ../../compare_directory.py ../touchstone_init ./
 
 segway "$cluster_arg" identify-run "../simpleseg.genomedata" traindir identifydir
 
-python${SEGWAY_TEST_PYTHON_VERSION:-""} ../touchstone_run ./
+python${SEGWAY_TEST_PYTHON_VERSION:-""} ../../compare_directory.py ../touchstone_run ./
     
 segway "$cluster_arg" identify-finish "../simpleseg.genomedata" traindir identifydir
 
-python${SEGWAY_TEST_PYTHON_VERSION:-""} ../touchstone_finish ./
+python${SEGWAY_TEST_PYTHON_VERSION:-""} ../../compare_directory.py ../touchstone_finish ./
