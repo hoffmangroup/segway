e9a945fb56d85309193c7507247351baa2010921
Eric Roberts <eroberts@uhnresearch.ca>
2020-03-06 12:36:50 -0500

    Update sphinx HTML math image extension to be compatible with versions > 1.8

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ from setup import __version__ as release
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.pngmath', 'sphinx.ext.todo']
+extensions = ['sphinx.ext.imgmath', 'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
