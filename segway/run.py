9b376e81e03bac79c8a03967640f9260959c2b73
Isaiah Hazelwood <isaiah.hazelwood@mail.utoronto.ca>
2024-05-30 16:27:10 -0400

    Move testing to generate input.master into save_input_master

--- segway/run.py
+++ segway/run.py
@@ -2047,15 +2047,8 @@ class Runner(object):
         #     InputMasterSaver(self)(input_master_filename, 
         #                            self.params_dirpath, self.clobber, 
         #                            instance_index)
-        if input_master_filename:
-            is_new = self.clobber or not Path(input_master_filename).exists()
-        else:
-            input_master_filename = make_default_filename("input.master.tmpl", self.params_dirpath, instance_index)
-            is_new = True
-
-        if is_new:
-            save_input_master(self, input_master_filename, self.params_dirpath,
-                            self.clobber, instance_index)
+        save_input_master(self, input_master_filename, self.params_dirpath,
+                          instance_index)
 
     def load_supervision(self):
         # The semi-supervised mode changes the DBN structure so there is an
@@ -3240,15 +3233,8 @@ class Runner(object):
         # input_master_filename, input_master_filename_is_new = \
         #     InputMasterSaver(self)(self.input_master_filename,
         #                            self.params_dirpath, self.clobber)
-        if self.input_master_filename:
-            is_new = self.clobber or not Path(self.input_master_filename).exists()
-        else:
-            self.input_master_filename = make_default_filename("input.master.tmpl", self.params_dirpath, None)
-            is_new = True
-
-        if is_new:
-            save_input_master(self, self.input_master_filename, self.params_dirpath,
-                              self.clobber, None)
+        save_input_master(self, self.input_master_filename, self.params_dirpath,
+                          None)
 
         # save file locations to tab-delimited file
         self.save_tab_file(self, TRAIN_OPTION_TYPES, TRAIN_FILEBASENAME)
--- segway/segway_input_master.py
+++ segway/segway_input_master.py
@@ -10,6 +10,7 @@ __version__ = "$Revision$"
 from math import frexp, ldexp
 from string import Template
 import sys
+from path import Path
 
 from genomedata._util import fill_array
 from numpy import (array, empty, float32, outer, set_printoptions, sqrt, tile,
@@ -17,7 +18,7 @@ from numpy import (array, empty, float32, outer, set_printoptions, sqrt, tile,
 from six.moves import map, range
 
 from ._util import (copy_attrs, data_string, DISTRIBUTION_ASINH_NORMAL,
-                    DISTRIBUTION_GAMMA, DISTRIBUTION_NORM,
+                    DISTRIBUTION_GAMMA, DISTRIBUTION_NORM, extjoin_not_none,
                     make_default_filename, OFFSET_END, OFFSET_START,
                     OFFSET_STEP, resource_substitute, Saver, SEGWAY_ENCODING,
                     SUPERVISION_SEMISUPERVISED,
@@ -72,7 +73,8 @@ DISTRIBUTIONS_LIKE_NORM = frozenset([DISTRIBUTION_NORM,
 # TODO[PY2-EOL]: remove
 ROUND_NDIGITS = 12
 
-INPUT_MASTER_NAME = "input.master.tmpl"
+INPUT_MASTER_PREFIX = "input"
+INPUT_MASTER_EXT = "master"
 
 
 def vstack_tile(array_like, *reps):
@@ -164,10 +166,24 @@ jitter = vectorize(jitter_cell)
 
 
 def save_input_master(runner, input_master_filename, params_dirpath=None,
-                      clobber=False, instance_index=None):
+                      instance_index=None):
     """
     Save the input.master file using the GMTK API.
     """
+    # Determine if the given input.master file already exits. 
+    # If input_master_filename is None, use the default name input.master
+    if input_master_filename:
+        is_new = runner.clobber or not Path(input_master_filename).exists()
+    else:
+        input_master_filebasename = \
+            extjoin_not_none(INPUT_MASTER_PREFIX, instance_index, INPUT_MASTER_EXT)
+        input_master_filename = Path(runner.params_dirpath) / input_master_filebasename
+        is_new = True
+
+    # If the input.master file already exits, do not create it and exit
+    if not is_new:
+        return
+    # Otherwise, create the new input.master file
 
     # Preamble
     include_filename = runner.gmtk_include_filename_relative
