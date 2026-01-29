e8428aaecd0e11cf09fdb487d754cb9d088788e9
Eric Roberts <notthebmovieactor@gmail.com>
2026-01-29 10:29:26 -0500

    Fix pathlib and pkg_resources deprecations (#183)
    * Fix pathlib and pkg_resources deprecations
    * Update deprecated upload artifacts github action
    * Move continuous integration testing to Python 3.10 to match PyTables
    * Remove more pkg_resources imports and pathlib deprecation fix
    * Replace deprecated listdir method for Path
    * Replace deprecated Path isfile method with is_file
    * Update news with deprecation fixes
    * Use version parsing from python packaging to check GMTK version

--- segway/_util.py
+++ segway/_util.py
@@ -9,9 +9,11 @@ from collections import defaultdict, namedtuple
 from contextlib import closing
 from functools import partial
 from gzip import open as _gzip_open
+from importlib import resources
 from itertools import repeat
 from math import floor, log10
 from os import extsep
+from packaging.version import parse as parse_version
 import re
 from string import Template
 import sys
@@ -20,7 +22,6 @@ import colorbrewer
 from numpy import (append, array, empty, insert, intc, maximum, str_, zeros)
 from optbuild import Mixin_UseFullProgPath, OptionBuilder_ShortOptWithSpace_TF
 from path import Path
-from pkg_resources import resource_filename, resource_string
 from six import viewitems
 from six.moves import map, zip
 from tables import Filters, NoSuchNodeError, open_file
@@ -94,13 +95,6 @@ OFFSET_START = 0
 OFFSET_END = 1
 OFFSET_STEP = 2
 
-data_filename = partial(resource_filename, PKG_DATA)
-
-
-def data_string(resource):
-    return resource_string(PKG_DATA, resource).decode(SEGWAY_ENCODING)
-
-
 NUM_COLORS = 8
 SCHEME = colorbrewer.Dark2[NUM_COLORS]
 
@@ -133,6 +127,24 @@ SUPERVISION_SEMISUPERVISED = 1
 SUPERVISION_SUPERVISED = 2
 
 SUPERVISION_LABEL_OFFSET = 1
+
+
+def data_filename(resource):
+    return str(resources.files(PKG_DATA) / resource)
+
+
+def data_string(resource):
+    return resources.read_text(PKG_DATA, resource, encoding=SEGWAY_ENCODING)
+
+
+def parse_gmtk_version(version_string):
+    """Returns a tuple of 3 integers of from the GMTK version string.
+        e.g. input: "1.4.2"
+             output: (1, 4, 2)
+    """
+
+    # NB: the release attribute contains the version tuple
+    return parse_version(version_string).release
 
 
 def extjoin(*args):
